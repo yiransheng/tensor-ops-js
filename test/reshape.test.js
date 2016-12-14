d392d5692c40b6b40146db554968b020660eecd8
Yiran Sheng <shengyiran@gmail.com>
2016-12-14 10:45:16 -0800

    remove log

--- test/reshape.test.js
+++ test/reshape.test.js
@@ -18,8 +18,6 @@ test("reshape tensors", t => {
     [[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11],
      [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]];
 
-  console.log(reshape(input, [2,12]), "RESHAPE");
-
   t.deepEqual(reshape(input, [2,3,4]), output1);
   t.deepEqual(reshape(input, [2,12]), output2);
 });
